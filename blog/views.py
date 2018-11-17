e8d6b0f8df6d1d4595740ebf856f433d993cd80f
Marie Klaus <marie.klaus@code.berlin>
2018-11-17 15:44:32 +0100

    Add blog to website.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blogpost
 
-# Create your views here.
+def allblogs(request):
+    blogposts = Blogpost.objects
+    return render(request, 'blog/allblogs.html', {'blogposts': blogposts})
+
+def detail(request, blog_id):
+    details = get_object_or_404(Blogpost, pk=blog_id)
+    return render(request, 'blog/detail.html', {'details': details})
